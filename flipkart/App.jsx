139c8226480fde4bec308bc8c9a53d3c5ab74ee3
Aswinshaji0001 <aswinshaji0001@gmail.com>
2024-11-04 09:05:00 +0530

    added

--- flipkart/App.jsx
+++ flipkart/App.jsx
@@ -1,6 +1,7 @@
 import React,{Component} from 'react'
 import './App.css'
 
+
 class App extends Component{
   constructor(props){
     super(props);
@@ -15,8 +16,26 @@ class App extends Component{
     this.setState({items:[...data.products]})
   }
   render(){
+    
     console.log(this.state.items);
-    return <div className="main">
+    return <>
+    <nav>
+      <div className="n1">
+      <h1>Flipkart.com</h1>
+      </div>
+      <div className="n2">
+        <input type="text" className='search'/>
+      </div>
+      <div className="n3">
+        <img src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/profile-52e0dc.svg" alt=""/>
+        <h3>Login</h3>
+      </div>
+      <div className="n4">
+        <img src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/header_cart-eed150.svg" alt="" />
+        <h3>Cart</h3>
+      </div>
+    </nav>
+    <div className="main">
       {this.state.items.map((item)=>{
         return <div className="item">
           <h1>{item.title.substring(0,15)}</h1>
@@ -24,6 +43,7 @@ class App extends Component{
           </div>
       })}
     </div>
+    </> 
   }
 }
 
