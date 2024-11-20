9fa64e00103a76461c8cd8a2bfd4e5aa1ee1ef88
Aswinshaji0001 <aswinshaji0001@gmail.com>
2024-11-20 08:43:17 +0530

    Added

--- customhook/src/App.jsx
+++ customhook/src/App.jsx
@@ -1,15 +1,15 @@
 
-import './App.css'
+import './App.scss'
 import useCustomHook from './useCustomHook'
 function App() {
 
   const {val,increment,decrement}=useCustomHook(0)
   return (
     <>
-        <div>
-          <button onClick={increment}>+</button>
+        <div className='container'>
+          <button className='btn1' onClick={increment}>+</button>
           <span>{val}</span>
-          <button onClick={decrement}>-</button>
+          <button className='btn2' onClick={decrement}>-</button>
 
         </div>
     </>
